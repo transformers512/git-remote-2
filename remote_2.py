bb9a941e76677fa2170a618ff49e0773aac85f60
transformers512 <hamidmalikafu@gmail.com>
2024-06-01 13:56:09 +0700

    menambahkan output print baru pada file remote_2.py

--- remote_2.py
+++ remote_2.py
@@ -3,3 +3,6 @@ def pangkat(n):
 
 hasil_pangkat = pangkat(3)
 print(f"hasil dari 6 pangkat 3 adalah: {hasil_pangkat(6)}")
+
+hasil_pangkat2 = pangkat(5)
+print(f"hasil dari 5 pangkat 5 adalah: {hasil_pangkat2(5)}")
